c977ab4e78a1219157f37d82264a007c842f953b
DivakovDev <divakovwork@gmail.com>
2024-07-25 16:07:32 +0300

    finish creating login and signin auth

---
+++ src/app/(auth)/signup/SignUpForm.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Form,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { signUpSchema, SignUpValues } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState, useTransition } from "react";
+import { useForm } from "react-hook-form";
+import { signUp } from "./action";
+import { PasswordInput } from "@/components/ui/PasswordInput";
+
+const SingUpForm = () => {
+
+    const [error, setError] = useState<string>();
+    const [isPending, startTransition] = useTransition();
+
+  const form = useForm<SignUpValues>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      email: "",
+      username: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: SignUpValues) {
+    setError(undefined);
+    startTransition(async () => {
+        const { error } = await signUp(values);
+        if(error) setError(error)
+    })
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+        {error && <p className='text-center text-destructive'>{error}</p>}
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="Username" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Email" type="email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <PasswordInput placeholder="Password" {...field}/>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full">
+          Create account
+        </Button>
+      </form>
+    </Form>
+  );
+};
+export default SingUpForm;
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import signupImage from "@/assets/signup-image.jpg";
 import Image from "next/image";
 import Link from "next/link";
+import SignUpForm  from './SignUpForm';
 
 export const metadata: Metadata = {
   title: "Sign Up",
@@ -19,7 +20,7 @@ const Page = () => {
             </p>
           </div>
           <div className='space-y-5'>
-            sign up form
+            <SignUpForm/>
             <Link href="/login" className='block text-center'>
             Already have an account? <span className='hover:underline font-semibold'>Log In</span></Link>
           </div>
@@ -27,7 +28,7 @@ const Page = () => {
 
         <Image
           src={signupImage}
-          alt="Login form image"
+          alt="Sign up form image"
           className="w-1/2 hidden object-cover md:block"
         />
       </div>
