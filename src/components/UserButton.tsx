717885be10bd4e991f64ede382ee601ebd53cd26
DivakovDev <divakovwork@gmail.com>
2024-08-03 19:27:49 +0300

    Adding ReactQuerry to make posts load dynamicly

--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -19,6 +19,7 @@ import { Check, LogOutIcon, Monitor, Moon, Sun, UserIcon } from "lucide-react";
 import { logout } from "@/app/(auth)/actions";
 import { cn } from "@/lib/utils";
 import { useTheme } from "next-themes";
+import { useQueryClient } from "@tanstack/react-query";
 
 interface UserButtonProps {
   className?: string;
@@ -28,6 +29,8 @@ const UserButtom = ({ className }: UserButtonProps) => {
   const { user } = useSession();
 
   const { theme, setTheme } = useTheme();
+
+  const queryClient = useQueryClient()
 
   return (
     <DropdownMenu>
@@ -73,6 +76,7 @@ const UserButtom = ({ className }: UserButtonProps) => {
         <DropdownMenuSeparator />
         <DropdownMenuItem
           onClick={() => {
+            queryClient.clear();
             logout();
           }}
         >
