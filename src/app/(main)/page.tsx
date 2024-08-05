6ae0a326a4369dad4b94f9ac6672068683e6289c
DivakovDev <divakovwork@gmail.com>
2024-08-05 18:30:35 +0300

    create and apply new feed for peoples who you follow to see only their posts and change a little bit theme and shadcn/ui components

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,8 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
 import TrendsSidebar from "@/components/TrendsSidebar";
 import ForYouFeed from "./ForYouFeed";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import FollowingFeed from "./FollowingFeed";
 
 export default function Home() {
 
@@ -8,7 +10,18 @@ export default function Home() {
     <div className="w-full min-w-0 flex gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
-        <ForYouFeed/>
+        <Tabs defaultValue="for-you">
+          <TabsList>
+            <TabsTrigger value="for-you">For you</TabsTrigger>
+            <TabsTrigger value="following">Following</TabsTrigger>
+          </TabsList>
+          <TabsContent value="for-you">
+            <ForYouFeed/>
+          </TabsContent>
+          <TabsContent value="following">
+            <FollowingFeed/>
+          </TabsContent>
+        </Tabs>
       </div>
       <TrendsSidebar/>
     </div>
