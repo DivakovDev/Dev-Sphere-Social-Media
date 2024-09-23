d44134e5ef2e87942db6c4a8ac6ce09a8d88be36
DivakovDev <divakovwork@gmail.com>
2024-09-23 20:04:33 +0300

    fix auth file

--- src/auth.ts
+++ src/auth.ts
@@ -1,80 +1,85 @@
-import { PrismaAdapter } from '@lucia-auth/adapter-prisma';
-import prisma from './lib/prisma';
-import { Lucia, Session, User } from 'lucia';
-import { cache } from 'react';
+import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
+import { Google } from "arctic";
+import { Lucia, Session, User } from "lucia";
 import { cookies } from "next/headers";
+import { cache } from "react";
+import prisma from "./lib/prisma";
 
 const adapter = new PrismaAdapter(prisma.session, prisma.user);
 
 export const lucia = new Lucia(adapter, {
-    sessionCookie:{
-        expires:false,
-        attributes:{
-            secure:process.env.NODE_ENV == "production"
-        }
+  sessionCookie: {
+    expires: false,
+    attributes: {
+      secure: process.env.NODE_ENV === "production",
     },
-    getUserAttributes(databaseUserAttributes){
-        return {
-            id: databaseUserAttributes.id,
-            username: databaseUserAttributes.username,
-            displayName: databaseUserAttributes.displayName,
-            avatarUrl: databaseUserAttributes.avatarUrl,
-            googleId: databaseUserAttributes.googleId,
-        }
-    },
-})
+  },
+  getUserAttributes(databaseUserAttributes) {
+    return {
+      id: databaseUserAttributes.id,
+      username: databaseUserAttributes.username,
+      displayName: databaseUserAttributes.displayName,
+      avatarUrl: databaseUserAttributes.avatarUrl,
+      googleId: databaseUserAttributes.googleId,
+    };
+  },
+});
 
 declare module "lucia" {
-    interface Register{
-        Lucia: typeof lucia;
-        DatabaseUserAttributes: DatabaseUserAttributes;
-    }
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
 }
 
-interface DatabaseUserAttributes{
-    id: string,
-    username:string,
-    displayName:string,
-    avatarUrl:string|null,
-    googleId:string|null,
+interface DatabaseUserAttributes {
+  id: string;
+  username: string;
+  displayName: string;
+  avatarUrl: string | null;
+  googleId: string | null;
 }
+
+export const google = new Google(
+  process.env.GOOGLE_CLIENT_ID!,
+  process.env.GOOGLE_CLIENT_SECRET!,
+  `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/callback/google`,
+);
 
 export const validateRequest = cache(
-    async() : Promise<
-    { user:User; session:Session } | {user:null; session:null}
-    > => {
-        const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
-    
-        if(!sessionId){
-            return{
-                user:null,
-                session:null,
-            }
-        }
+  async (): Promise<
+    { user: User; session: Session } | { user: null; session: null }
+  > => {
+    const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
 
-        const result = await lucia.validateSession(sessionId);
+    if (!sessionId) {
+      return {
+        user: null,
+        session: null,
+      };
+    }
 
-        try {
-            if(result.session && result.session.fresh){
-                const sessionCookie = lucia.createSessionCookie(result.session.id)
-                cookies().set(
-                    sessionCookie.name,
-                    sessionCookie.value,
-                    sessionCookie.attributes,
-                )
-            }
+    const result = await lucia.validateSession(sessionId);
 
-            if(!result.session){
-                const sessionCookie = lucia.createBlankSessionCookie();
+    try {
+      if (result.session && result.session.fresh) {
+        const sessionCookie = lucia.createSessionCookie(result.session.id);
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes,
+        );
+      }
+      if (!result.session) {
+        const sessionCookie = lucia.createBlankSessionCookie();
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes,
+        );
+      }
+    } catch {}
 
-                cookies().set(
-                    sessionCookie.name,
-                    sessionCookie.value,
-                    sessionCookie.attributes,
-                );
-            }
-        } catch {}
-
-        return result;
-    },
-)
+    return result;
+  },
+);
